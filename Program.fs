ae2df1424af4c7321cfb091dd7fbeb4430c50c99
Vladislav Prekel <misterptits@yandex.ru>
2021-04-10 09:53:34 +0700

    2. Output an Image

--- Program.fs
+++ Program.fs
@@ -1,13 +1,25 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
-open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+﻿open System.IO
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main _ =
+    let imageWidth = 256
+    let imageHeight = 256
+
+    use sw =
+        new StreamWriter(Path.Combine(__SOURCE_DIRECTORY__, "image.ppm"))
+
+    fprintfn sw $"P3\n%d{imageWidth} %d{imageHeight}\n255"
+
+    for j in [ imageHeight - 1 .. -1 .. 0 ] do
+        for i in [ 0 .. imageWidth - 1 ] do
+            let r = double i / double (imageWidth - 1)
+            let g = double j / double (imageHeight - 1)
+            let b = 0.25
+
+            let ir = int (255.999 * r)
+            let ig = int (255.999 * g)
+            let ib = int (255.999 * b)
+
+            fprintfn sw $"%d{ir} %d{ig} %d{ib}"
+
+    0
