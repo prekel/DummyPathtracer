f362eb90597f9c74686a49f4c17515ab06ac7fa0
Vladislav Prekel <misterptits@yandex.ru>
2021-04-10 18:24:09 +0700

    7.18. Some Random Number Utilities

--- Utils.fs
+++ Utils.fs
@@ -3,4 +3,10 @@ module DummyPathtracer.Utils
 
 open System
 
+let random = Random()
+
 let degreeToRadians degrees = degrees * MathF.PI / 180.f
+
+let randomDouble () = random.NextDouble() |> float32
+
+let randomDoubleMinMax min max = min + (max - min) * randomDouble ()
